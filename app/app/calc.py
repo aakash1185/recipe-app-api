f0b779fe12afa94f8c352e0c56c5a88c5c13789e
aakash <aakashbhardwaj04@gmail.com>
2024-12-16 22:10:37 +0530

    test driven boilerplate added

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculate functions
+"""
+
+def add(x, y):
+    """ Add x and y return result."""
+    return x+y
+
+def subtract(x, y):
+    """ Add x and y return result."""
+    return y - x
