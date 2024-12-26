faa54ffd30f1ddf3c4edce1a9466dd9a648b1b70
aakash <aakashbhardwaj04@gmail.com>
2024-12-26 22:28:45 +0530

    configured docker compose and checks to wait for db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -7,7 +7,7 @@ from unittest.mock import patch
 from psycopg2 import OperationalError, Error as Psycopg2Error
 
 from django.core.management import call_command
-from django.db.utils import OperationalError
+
 from django.test import SimpleTestCase
 
 
